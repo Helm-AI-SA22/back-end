33a82e1cf0121a3c1175b03fcc78b0098ce478cd
Luca Moroni <terraformer.144@gmail.com>
2022-10-27 19:20:11 +0200

    minor changes

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h1>Hello from BE MODULE</h1>'
 
 
 if __name__ == "__main__":
