5af0b7a67f679c9b6bb71359ba5e6132d8370f72
frankpietro <pfrancaviglia@gmail.com>
2022-10-28 17:52:48 +0200

    Added utils folder

---
+++ utils/constants.py
@@ -0,0 +1,3 @@
+SCOPUS_SITE = 'https://api.elsevier.com/content/search/scopus'
+SCOPUS_API_KEY = '3e68f6817fc5ecd448df31067d4bd08a'
+SCOPUS_HEADERS = 'apiKey=' + SCOPUS_API_KEY + '&httpAccept=application/json'
