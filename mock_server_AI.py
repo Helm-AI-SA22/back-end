41f224989262cb2851d7cc69c19ac1c8fbefd375
frankpietro <pfrancaviglia@gmail.com>
2022-10-28 17:52:12 +0200

    Code refactoring
    Co-authored-by: Giacomo Cignoni <giacomo-cgn@users.noreply.github.com>
    Co-authored-by: VeronicaPistolesi <VeronicaPistolesi@users.noreply.github.com>

--- mock_server_AI.py
+++ mock_server_AI.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-import json
+
 
 def run_server_mock_AI(host, port):
     class BackendServer(Resource):
@@ -9,20 +9,14 @@ def run_server_mock_AI(host, port):
             return {'prova AI': 'prova get'}
             
 
-
         def post(self):
             req_json = request.get_json()
             print('Json arrived to AI:', req_json)
-
-            return_json = {'prova AI': 'prova prova prova vnslofnsw'}
-
+            return_json = {'prova AI': 'riuscita'}
             return jsonify(return_json)
-
-
 
 
     app = Flask(__name__)
     api = Api(app)
     api.add_resource(BackendServer, "/")
     app.run(host = host, port = port)
-
