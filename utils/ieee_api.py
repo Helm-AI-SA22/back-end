9fff42dc4ca62022d95da2a92c9dd3c57aa909fb
frankpietro <pfrancaviglia@gmail.com>
2022-11-02 15:25:11 +0100

    Added ieee constants and basic API (not tested)

---
+++ utils/ieee_api.py
@@ -0,0 +1,14 @@
+from utils.constants import *
+import requests
+import json
+
+
+# define ieee request for api
+def compose_ieee_request(q_str):
+    return IEEE_SITE + '?querytext=' + q_str + '&' + IEEE_HEADERS
+
+# execute request
+def make_ieee_request(q_str):
+    ieee_request = compose_ieee_request(q_str)
+    ieee_response = requests.get(ieee_request).json()
+    return ieee_response
