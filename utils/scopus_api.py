dd5d72792086ce501a79826d03b57cbcb8fa1252
Luca Moroni <terraformer.144@gmail.com>
2022-11-05 16:56:32 +0100

    cleaned scopus features

--- utils/scopus_api.py
+++ utils/scopus_api.py
@@ -2,6 +2,26 @@ import requests
 from utils.constants import *
 
 
+def clear_author(author_paper):
+
+    res_authors = list()
+
+    for aut in author_paper:
+        res_authors.append(aut["authname"])
+
+    return ";".join(res_authors)
+
+
+def clear_link(link_paper):
+    return link_paper[1]["@href"]
+
+
+def clear_features(list_papers):
+    for paper in list_papers:
+        paper["author"] = clear_author(paper["author"])
+        paper["link"] = clear_author(paper["link"])
+
+
 def compose_scopus_request(q_str, start=0):
     return SCOPUS_SITE + '?query=' + q_str + '&' + SCOPUS_HEADERS + f"&start={start}"
 
@@ -27,5 +47,8 @@ def make_scopus_request(q_str):
 
         except Exception as e:
             return e.message()
+    
+    # clear features to make them consistent with other sources
+    result = clear_features(result)
 
     return result
