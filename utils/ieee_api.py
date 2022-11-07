3bb771b6b27fdf3eed9830cd0985cb8027568435
Alessandro <capurso89@gmail.com>
2022-11-07 18:12:26 +0100

    create query string for IEEE create request for ieee papers retrieving

--- utils/ieee_api.py
+++ utils/ieee_api.py
@@ -18,22 +18,30 @@ def compose_ieee_request(q_str):
     return IEEE_SITE + '?querytext=' + q_str + '&' + IEEE_HEADERS
 
 # execute request
-def make_ieee_request(q_str):
-    # ieee_request = compose_ieee_request(q_str)
-    # ieee_response = requests.get(ieee_request).json()
+def make_ieee_request(keywords):
+    
+    key = []
+    for keyword in keywords:
+        key.append(QUOTES_IEEE + keyword.replace(' ', SPACE_IEEE) + QUOTES_IEEE)
+    keys_conj = SPACE_IEEE + "AND" + SPACE_IEEE
+    query_string = keys_conj.join(key)
+    
+    ieee_request = compose_ieee_request(query_string)
+    ieee_response = requests.get(ieee_request).json()
 
+    """
     with open("mocks/ieee.json") as f:
         ieee_response = json.load(f)
+    """
 
-    # transform ieee response in a list of papers, with relative infoes
+    # transform ieee response in a list of papers, with relative infos
 
     transfomed_ieee_response = defaultdict(dict)
 
     for feature_key, feature_values_dict in ieee_response.items():
         for key, value in feature_values_dict.items():
             transfomed_ieee_response[key][feature_key] = value
-
+    
     transfomed_ieee_response = list(transfomed_ieee_response.values())
-
 
     return remove_nodoi(transfomed_ieee_response)
