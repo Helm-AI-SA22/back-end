8c16a731d4c1aff446efb4bf1ea512be63ca18db
Gabriele Marino <88272671+GM862001@users.noreply.github.com>
2022-11-18 14:58:56 +0100

    Update ranking.py

--- utils/ranking.py
+++ utils/ranking.py
@@ -5,4 +5,9 @@ def rank(documents, sort_key):
 	if not sort_key in ["publicationDate", "citationCount", "tfidf"]:
 		error_log("Only 'publicationDate', 'tfidf' and 'citationCount' can be used as sorting keys")
 	
-	return sorted(documents, key = lambda x: -int(x[sort_key]))
+	if sort_key == "tfidf":
+		return sorted(documents, key = lambda x: -float(x[sort_key]))
+	elif sort_key == "citationCount":
+		return sorted(documents, key = lambda x: -int(x[sort_key]))
+	elif sort_key == "publicationDate":
+		return sorted(documents, key = lambda x: -(int(re.findall(r"(\d{4})", x["publicationDate"])[0])))
