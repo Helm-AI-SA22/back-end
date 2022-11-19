70192991995783d024516510c95a7efde7074cbc
Luca Moroni <terraformer.144@gmail.com>
2022-11-19 21:01:03 +0100

    moved keys

--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,10 @@
+from keys import SCOPUS_API_KEY, IEEE_API_KEY
+
 SCOPUS_SITE = 'https://api.elsevier.com/content/search/scopus'
-SCOPUS_API_KEY = '3e68f6817fc5ecd448df31067d4bd08a'
 SCOPUS_COUNT = 25
 SCOPUS_MAX_COUNT = 25
 SCOPUS_HEADERS = 'apiKey=' + SCOPUS_API_KEY + '&httpAccept=application/json&view=COMPLETE' + '&count=' + str(SCOPUS_COUNT)
 
-IEEE_API_KEY = '5tn9eufwb5f2j8wm3ed9esaj'
 IEEE_SITE = 'https://ieeexploreapi.ieee.org/api/v1/search/articles'
 IEEE_HEADERS = 'apikey=' + IEEE_API_KEY
 
