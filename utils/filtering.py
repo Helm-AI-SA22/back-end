0348892566725a343fb660d4ced1cf391b0caafe
frankpietro <pfrancaviglia@gmail.com>
2022-12-20 18:13:19 +0100

    Changed some strings into constants

--- utils/filtering.py
+++ utils/filtering.py
@@ -1,22 +1,22 @@
 import pandas as pd
 from utils.utils import add_topic_ratio
 
-MIN = 'min'
-MAX = 'max'
 
 UNION = 'union'
 INTERSECTION = 'intersection'
-
+MODE = 'mode'
+ID = 'id'
+TOPICS = 'topics'
 
 # mode = 'union' --> OR
 # mode = 'intersection' --> AND
 def topic_filtering(data_df, topic_dict):
     # OR case
-    if topic_dict["mode"] == UNION:
+    if topic_dict[MODE] == UNION:
         for i, row in data_df.iterrows():
             drop = True
             for topic in row.topics:
-                if topic['id'] in topic_dict["topics"]:
+                if topic[ID] in topic_dict[TOPICS]:
                     drop = False
                     break
 
@@ -24,45 +24,55 @@ def topic_filtering(data_df, topic_dict):
                 data_df.drop(i, inplace=True)  
 
     # AND case
-    elif topic_dict["mode"] == INTERSECTION:
+    elif topic_dict[MODE] == INTERSECTION:
         for i, row in data_df.iterrows():
-            for topic in topic_dict["topics"]:
-                if topic not in [t['id'] for t in row.topics]:
+            for topic in topic_dict[TOPICS]:
+                if topic not in [t[ID] for t in row.topics]:
                     data_df.drop(i, inplace=True)  
                     break
                 
     return data_df
 
 
+MIN = 'min'
+MAX = 'max'
+YEAR = 'year'
+PUB_DATE = 'publicationDate'
+
 def date_filtering(data_df, crit):
     # add data_df['year'] column finding 4 digits string in data_df['publicationDate']
-    data_df['year'] = data_df['publicationDate'].str.extract('(\d{4})', expand=True).astype(int)
-    data_df = data_df[data_df['year'] >= crit[MIN]]
+    data_df[YEAR] = data_df[PUB_DATE].str.extract('(\d{4})', expand=True).astype(int)
+    data_df = data_df[data_df[YEAR] >= crit[MIN]]
 
     if crit[MAX] != -1:
-        data_df = data_df[data_df['year'] <= crit[MAX]]
+        data_df = data_df[data_df[YEAR] <= crit[MAX]]
 
     # drop data_df['year'] column
-    data_df.drop('year', axis=1, inplace=True)
+    data_df.drop(YEAR, axis=1, inplace=True)
 
     return data_df
 
+
+AUTHORS = 'authors'
 
 def author_filtering(data_df, crit):
     # drop rows of data_df where crit_string is not in data_df['authors']
-    data_df['authors']=data_df['authors'].str.lower()
+    data_df[AUTHORS] = data_df[AUTHORS].str.lower()
     for author in crit:
         print(author)
-        data_df = data_df[data_df['authors'].str.contains(author.lower())]
-    data_df['authors']=data_df['authors'].str.title()
+        data_df = data_df[data_df[AUTHORS].str.contains(author.lower())]
+
+    data_df[AUTHORS] = data_df[AUTHORS].str.title()
     return data_df
 
 
+CIT_COUNT = 'citationCount'
+
 def citation_filtering(data_df, crit):
-    data_df = data_df[data_df['citationCount'] >= crit[MIN]]
+    data_df = data_df[data_df[CIT_COUNT] >= crit[MIN]]
     
     if crit[MAX] != -1:
-        data_df = data_df[data_df['citationCount'] <= crit[MAX]]
+        data_df = data_df[data_df[CIT_COUNT] <= crit[MAX]]
 
     return data_df
 
@@ -72,18 +82,21 @@ def citation_filtering(data_df, crit):
 # 1 = non-arxiv only
 def arxiv_filtering(data_df, crit):
     if (crit == 1):
-        data_df = data_df[data_df['citationCount'] == -1]
+        data_df = data_df[data_df[CIT_COUNT] == -1]
     else:
-        data_df = data_df[data_df['citationCount'] >= -1]
+        data_df = data_df[data_df[CIT_COUNT] >= -1]
 
     return data_df
+
+
+OPEN_ACCESS = 'openaccess'
 
 #Take documents with 'openaccess' = 'availability' (null for both)
 #   null = all documents
 #   0 = free access (only)
 #   1 = pay access (only)
 def availability_filtering(data_df, flag):
-    data_df = data_df[data_df['openaccess'] == flag]
+    data_df = data_df[data_df[OPEN_ACCESS] == flag]
     return data_df
 
 
